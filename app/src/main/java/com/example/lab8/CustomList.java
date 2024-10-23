edd841df92d19f6046cd9932b2ce2d5ffd0fd5c5
Yeajin46 <yeajin3@ualberta.ca>
2024-10-22 20:10:41 -0600

    Create an object for CustomList, add the test method to test addCity method

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import org.jetbrains.annotations.TestOnly;
+import org.junit.Test;
+
 import java.util.ArrayList;
 
 public class CustomList extends ArrayAdapter<City> {
@@ -44,13 +47,21 @@ public class CustomList extends ArrayAdapter<City> {
         return view;
 
     }
-
+    /**
+     * This gets size of the list
+     * @return
+     */
     public int getCount(){
         return cities.size();
     }
 
+    /**
+     * this adds a city object to the list
+     * for the first phase it will be
+     * empty @param city
+     */
+
     public void addCity(City city){
 
     }
-
 }
